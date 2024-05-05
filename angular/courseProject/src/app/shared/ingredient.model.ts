c63ab49c7a45c63ac90c24bcfa4d6ac614847af6
Suleiman Imtiaz Ahmed <sahmed24@luc.edu>
2024-05-04 19:33:41 -0500

    Reformatting

--- angular/courseProject/src/app/shared/ingredient.model.ts
+++ angular/courseProject/src/app/shared/ingredient.model.ts
@@ -10,6 +10,6 @@
 
 // Other way to define model which is shortcut.
 // It will automatically give us properties with the names we specify here as argument names and will automatically assign the values we recieve in this constructor to these newly created properties.
-export class Ingredient{
-    constructor(public name: string, public amount: number){}
+export class Ingredient {
+    constructor(public name: string, public amount: number) { }
 }
